763ab3713002073a86cddb249a1e9c2efe17f559
minato tokuyama <167080378+toku01600@users.noreply.github.com>
2025-08-20 00:27:12 +0900

    修正

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/utils/authOptions';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { prisma } from '@/lib/prisma';
 
 export default async function AdminPage(){
