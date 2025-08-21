08e065b83813a4a3e7c8f541fc99a249767f32d8
minato tokuyama <167080378+toku01600@users.noreply.github.com>
2025-08-21 09:58:25 +0900

    修正および作成

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { handlers } from "@/lib/auth";
 
-export const { handlers: { GET, POST }, auth } = NextAuth(authOptions);
+export const { GET, POST } = handlers;
